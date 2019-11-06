f68fb1b72500e37460656b5f90f1f76619ffceb3
Mina W. Guirgis <minawagieh@gmail.com>
2019-11-06 14:30:01 +0200

    add firebase function
    add firebase function and deploy it

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -3,8 +3,18 @@ import React, {Component} from 'react';
 class Notifications extends Component {
     render() {
         return (
-            <div>
-               <h4>Notification</h4>
+            <div className="notification-section">
+               <div className="card z-depyh-0">
+                   <div className="card-content">
+                       <span className="card-title">Notification</span>
+                       <ul className="notification">
+                           <li>Notification</li>
+                           <li>Notification</li>
+                           <li>Notification</li>
+                           <li>Notification</li>
+                       </ul>
+                   </div>
+               </div>
             </div>
         );
     }
